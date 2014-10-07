43739b96a32d27191a5bc8c27f10dc81dcd3bc29
David Jorm <djorm@redhat.com>
2014-10-07 20:56:48 +1000

    Fix whitespace to conform with standards

--- src/adaptors/bms_adaptor.c
+++ src/adaptors/bms_adaptor.c
@@ -694,16 +694,16 @@ static void get_mtr_reading(bms_mtr_t *mtr, void *val)
 
 	switch (mtr->type) {
 	case MTR_TYPE_FLOAT:
-        	memcpy(val, (void *) &mtr->value.f, sizeof(float));
+        memcpy(val, (void *) &mtr->value.f, sizeof(float));
 		break;
 	case MTR_TYPE_UINT16:
-        	memcpy(val, (void *) &mtr->value.u16, sizeof(uint16_t));
+        memcpy(val, (void *) &mtr->value.u16, sizeof(uint16_t));
 		break;
 	case MTR_TYPE_UINT32:
-        	memcpy(val, (void *) &mtr->value.u32, sizeof(uint32_t));
+        memcpy(val, (void *) &mtr->value.u32, sizeof(uint32_t));
 		break;
 	case MTR_TYPE_BOOL:
-        	memcpy(val, (void *) &mtr->value.b, sizeof(LVL_MTR));
+        memcpy(val, (void *) &mtr->value.b, sizeof(LVL_MTR));
 		break;
 	default:
 		/*
